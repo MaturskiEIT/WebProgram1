e58a0d9d0afb1af0544fde76db5f0a97743ad8ba
MaturskiEIT <39279514+MaturskiEIT@users.noreply.github.com>
2018-06-04 18:50:15 +0200

    Add files via upload

---
+++ ZadatakA1/functions.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    $('#guska').click(function () {
+        alert('Guska je ptica srednje velicine iz porodice plovke.');
+    });
+    var guskaAudio = document.getElementById("guskaZvuk");
+    $('#guska').mouseover(function () {
+        guskaAudio.play();
+    });
+
+    $('#krava').click(function () {
+        alert('Krava je domace govedo.');
+    });
+    var kravaAudio = document.getElementById("kravaZvuk");
+    $('#krava').mouseover(function () {
+        kravaAudio.play();
+    });
+
+    $('#macka').click(function () {
+        alert('Macka je mali mesozder.');
+    });
+    var mackaAudio = document.getElementById("mackaZvuk");
+    $("#macka").mouseover(function () {
+        mackaAudio.play();
+    });
+
+    $('#pas').click(function () {
+        alert('Pas je pripitomljeni sisar iz porodice Canidae.');
+    });
+    var pasAudio = document.getElementById("pasZvuk");
+    $('#pas').mouseover(function () {
+        pasAudio.play();
+    });
+
+    $('#petao').click(function () {
+        alert('Petao je vrsta ptice, muzjak kokoske.');
+    });
+    var petaoAudio = document.getElementById("petaoZvuk");
+    $('#petao').mouseover(function () {
+        petaoAudio.play();
+    });
+
+
+
+});
